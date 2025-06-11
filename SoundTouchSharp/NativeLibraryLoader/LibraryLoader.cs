2fd521cde3039a634990061578b413446f8bc81e
cuip <msavoia@nifty.com>
2025-06-11 15:27:26 +0900

    for .net8, arm64

--- SoundTouchSharp/NativeLibraryLoader/LibraryLoader.cs
+++ SoundTouchSharp/NativeLibraryLoader/LibraryLoader.cs
@@ -3,45 +3,61 @@ using System.Runtime.InteropServices;
 
 namespace SoundTouchSharp.NativeLibraryLoader
 {
-    public class LibraryLoader
-    {
-        public static IntPtr LoadNativeLibrary(string name)
-        {
-            return CoreLoadNativeLibrary(name);
-        }
+   public class LibraryLoader
+   {
+      public static IntPtr LoadNativeLibrary(string name)
+      {
+         return RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+          ? LoadLibrary(name)
+          : dlopen(name, RTLD_NOW);
+      }
 
-        public void FreeNativeLibrary(IntPtr handle)
-        {
-            if (handle == IntPtr.Zero)
-            {
-                throw new ArgumentException("Parameter must not be zero.", nameof(handle));
-            }
+      public void FreeNativeLibrary(IntPtr handle)
+      {
+         if (handle == IntPtr.Zero)
+            throw new ArgumentException("Parameter must not be zero.", nameof(handle));
 
-            CoreFreeNativeLibrary(handle);
-        }
-        public static void CoreFreeNativeLibrary(IntPtr handle)
-        {
+         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             FreeLibrary(handle);
-        }
+         else
+            dlclose(handle);
+      }
 
-        public static IntPtr CoreLoadFunctionPointer(IntPtr handle, string functionName)
-        {
-            return GetProcAddress(handle, functionName);
-        }
+    
+      public static IntPtr CoreLoadFunctionPointer(IntPtr handle, string functionName)
+      {
+         return RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+             ? GetProcAddress(handle, functionName)
+             : dlsym(handle, functionName);
+      }
 
-        public static IntPtr CoreLoadNativeLibrary(string name)
-        {
-            return LoadLibrary(name);
-        }
+      public static IntPtr CoreLoadNativeLibrary(string name)
+      {
+         return LoadLibrary(name);
+      }
 
+      // Windows
 
-        [DllImport("kernel32")]
-        public static extern IntPtr LoadLibrary(string fileName);
+      [DllImport("kernel32")]
+      public static extern IntPtr LoadLibrary(string fileName);
 
-        [DllImport("kernel32")]
-        public static extern IntPtr GetProcAddress(IntPtr module, string procName);
+      [DllImport("kernel32")]
+      public static extern IntPtr GetProcAddress(IntPtr module, string procName);
 
-        [DllImport("kernel32")]
-        public static extern int FreeLibrary(IntPtr module);
-    }
+      [DllImport("kernel32")]
+      public static extern int FreeLibrary(IntPtr module);
+
+      // macOS 
+      private const int RTLD_NOW = 2;
+
+      [DllImport("libdl")]
+      private static extern IntPtr dlopen(string fileName, int flags);
+
+      [DllImport("libdl")]
+      private static extern IntPtr dlsym(IntPtr handle, string symbol);
+
+      [DllImport("libdl")]
+      private static extern int dlclose(IntPtr handle);
+
+   }
 }
---
+++ SoundTouchSharp/ReadMe.md
@@ -0,0 +1,2 @@
+﻿Based on SoundTouchSharp by fallingrust.
+Modified for .NET 8 and macOS support.
